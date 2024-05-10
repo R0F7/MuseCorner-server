125f7275fa6b4f72e050c28bc83b0b36feee221e
R0F7 <wwwrafikhan075@gmail.com>
2024-05-10 20:11:01 +0600

    added read operation with sort

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000
 
 const corsOptions = {
     origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
+        'http://localhost:5173',
+        'http://localhost:5174',
     ],
     credentials: true,
     optionSuccessStatus: 200,
-  }
+}
 
 //middleware
 app.use(cors(corsOptions))
@@ -19,10 +21,45 @@ app.use(express.json())
 
 
 
-app.get('/', (req,res) => {
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wezoknx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    try {
+        const blogCollection = client.db('MuseCornerDB').collection('blog')
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        app.get('/blog', async (req, res) => {
+            const result = await blogCollection.find().sort({createdAt: -1}).toArray()
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('Alive blog website server')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Blog Website server run on port:${port}`);
 })
